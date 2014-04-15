3c13a6a5d30752fe368f9b046983a78a92325525
Aaron Scherer <aequasi@gmail.com>
2014-04-14 22:54:20 -0700

    Initial Commit

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+rsync -avze 'ssh' output_prod/ root@lfgame.rs:/var/www/bldr.io/
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
