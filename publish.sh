5681a42c38a32f6c6ea7f78f4239bec329bfc49f
Aaron Scherer <aequasi@gmail.com>
2014-10-13 21:30:15 -0700

    Fixing installer

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-rsync -avze 'ssh' output_prod/ root@lfgame.rs:/var/www/bldr.io/
+rsync -avze 'ssh' output_prod/ root@squint.lfgame.rs:/var/www/bldr.io/
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
